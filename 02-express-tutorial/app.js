c0bdcf9f75d415e168cbbb7d0f8bd10ac3c64a53
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-04-07 23:11:53 +0100

    Completed HW Questions until refactoring step

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -12,24 +12,68 @@ app.use([logger]);
 app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
 app.get('/api/v1/people', (req,res) => {
-    res.status(200).json({ success:true, data:people })
+    return res
+    .status(200)
+    .json({ success: true, data: people })
+})
+
+app.post('/api/v1/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Please provide your name.' })
+    }
+    // people.push({ id: people.length + 1, name: req.body.name });
+    // res.status(201).json({ success: true, name: req.body.name });
+    res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/v1/postman/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res
+            .status(400)
+            .json({ success: false, message: 'Please provide your name.' })
+        }
+        res.status(201).send({ success: true, data: [...people, name] })
 })
 
 app.post('/login', (req, res) => {
     const {name} = req.body;
     if(name) {
-        return res.status(200).send(`Welcome ${name}`)
+        return res
+        .status(200)
+        .send(`Welcome ${name}`)
     }
     res.status(401).send('Please provide credentials')
-    res.send('POST')
 })
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
 
-app.get('/api/products', (req,res) => {
-    res.send('Products')
+    const person = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        return res
+            .status(404)
+            .json({ success: false, message: `No person with this id ${id}` })
+        }
+        const newPeople = people.map((person) => {
+            if(person.id === Number(id)) {
+                person.name = name
+            }
+            return person
+        })
+        res.status(200).send({ success: true, data: newPeople })
+
 })
+
 
 app.listen(3000, () => {
     console.log('Server is listening on Port 3000')
