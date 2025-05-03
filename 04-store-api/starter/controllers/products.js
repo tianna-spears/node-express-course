fb1158efc9d612c355ac5ef7f74f62257b26f770
tianna-spears <140460149+tianna-spears@users.noreply.github.com>
2025-05-03 18:13:40 +0100

    quick change to controllers/products.js logic

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -33,16 +33,14 @@ const getAllProducts = async (req, res) => {
         }
         const regEx = /\b(<|>|>=|=|<|<=)\b/g
         let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
-
         const options = ['price', 'rating']
         filters = filters.split(',').forEach((item) => {
-            const [ field, operator, value] = item.split('-')
+            const [ field, operator, value ] = item.split('-')
             if(options.includes(field)) {
                 queryObject[field] = { [operator]: Number(value) }
             }
         }) 
     }
-
 
     console.log(queryObject)
     let result = Product.find(queryObject)
